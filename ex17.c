f10afad91177f30d23c5d6d82d5509896c94ecec
Kirill Maksimov <netoneko@gmail.com>
2013-01-06 15:16:39 +0200

    data structures

--- ex17.c
+++ ex17.c
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_ROWS 512
+#define MAX_DATA 100
+
 void die(char *message) {
   if (errno) {
     perror(message);
@@ -13,6 +16,33 @@ void die(char *message) {
   }
 
   exit(1);
+}
+
+struct Address {
+  int id;
+  char name[MAX_DATA];
+  char email[MAX_DATA];
+};
+
+struct Database {
+  struct Address rows[MAX_ROWS];
+};
+
+struct Connection {
+  FILE *file;
+  struct Database *db;
+};
+
+struct Connection *Connection_create(char *dbname) {
+  struct Connection *conn = malloc(sizeof(struct Connection));
+  if (!conn) die("Memory error");
+
+  return conn;
+}
+
+void Connection_destroy(struct Connection *connection) {
+  free(connection->db);
+  free(connection);
 }
 
 void print_input(char *dbfile, char *action, char *params[], int paramc) {
@@ -26,6 +56,12 @@ void print_input(char *dbfile, char *action, char *params[], int paramc) {
   }
 }
 
+void process_input(char *dbfile, char *action, char *params[], int paramc) {
+  struct Connection *conn = Connection_create(dbfile);
+
+  Connection_destroy(conn);
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 3) die("USAGE: ex17 <dbfile> <action> <params>");
 
@@ -35,6 +71,7 @@ int main(int argc, char *argv[]) {
   int paramc = argc - 3;
 
   print_input(dbfile, action, params, paramc);
+  process_input(dbfile, action, params, paramc);
 
   return 0;
 }
