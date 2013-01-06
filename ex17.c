c7c73d04cc0fe7a8c7fecce3a55548890c08c495
Kirill Maksimov <netoneko@gmail.com>
2013-01-06 14:54:46 +0200

    print input in distinct function

--- ex17.c
+++ ex17.c
@@ -15,21 +15,26 @@ void die(char *message) {
   exit(1);
 }
 
+void print_input(char *dbfile, char *action, char *params[], int paramc) {
+  printf("Input:\n");
+  printf("dbfile: %s\n", dbfile);
+  printf("action: %s\n", action);
+  printf("params: \n");
+
+  for (int i = 0; i < paramc; i++) {
+    printf("\t%s\n", params[i]);
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 3) die("USAGE: ex17 <dbfile> <action> <params>");
 
   char *dbfile = argv[1];
   char *action = argv[2];
   char **params = &argv[3];
+  int paramc = argc - 3;
 
-  printf("Input:\n");
-  printf("dbfile: %s\n", dbfile);
-  printf("action: %s\n", action);
-  printf("params: \n");
-
-  for (int i = 0; i < (argc - 3) ; i++) {
-    printf("\t%s\n", params[i]);
-  }
+  print_input(dbfile, action, params, paramc);
 
   return 0;
 }
